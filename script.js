9891007524b659c1d7febb6604b2a5d4677101e0
Dana ONeil <thelittlestschnauzer@gmail.com>
2016-12-16 15:22:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function () {
+(function (window) {
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
@@ -35,4 +35,4 @@ for (var i = 0; i < names.length; i++) {
     helloSpeaker.speak(names[i]);
   }
 }
-})();
+})(window);
