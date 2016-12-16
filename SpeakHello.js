e4cd64dafc3874207d3ce45247af8a8c0931a89e
Dana ONeil <thelittlestschnauzer@gmail.com>
2016-12-16 15:36:25 -0500

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,14 +1,17 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
-(function (window) {
+
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
- var helloSpeaker = {};
+ 
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
+(function (window) [
+ 
 var speakWord = "Hello";
+var helloSpeaker = {};
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
